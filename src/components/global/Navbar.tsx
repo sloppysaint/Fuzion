8e9ffcc324d80789b6799acfb974a4f68c631b6f
Piyush2604 <khattrapiyush80@gmail.com>
2024-09-15 21:36:35 +0530

    designing front page

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Link } from 'lucide-react'
+import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import { MenuIcon } from 'lucide-react'
 
 const Navbar = async () => {
   return <header className='fixed right-0 left-0 top-0 py-4 px-4
@@ -44,11 +45,21 @@ const Navbar = async () => {
     <aside className='flex items-center gap-4'>
         <Link 
             href='/dashboard'
-            className='relative inline-flex h-10'
+            className='relative inline-flex h-10 overflow-hidden rounded-full p-[2px]
+            focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'
         >
+            <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite]
+            bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393882_50%,#E2CBFF_100%)]'
+            />
+            <span className='inline-flex h-full w-full cursor-pointer items-center
+            justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
+                {//WIP: wire up user
+                true ? 'Dashboard':'Get Started'}
+            </span>
 
         </Link>
-
+            {/*wire up user  */}
+            <MenuIcon className='md:hidden'/>
     </aside>
   </header>
 }
